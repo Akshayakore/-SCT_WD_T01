6a4827a2ba364510342f4a9ea23f580b1650144c
Akshayakore <akshayakore05@gmail.com>
2024-10-24 23:04:02 +0530

    Add files via upload

---
+++ menu/script.js
@@ -0,0 +1,10 @@
+// Add a scroll event listener to the window
+window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.navbar');
+    // Add or remove the "scrolled" class based on scroll position
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
